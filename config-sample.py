d3c78dd865847a64e17d78ced488e0a16d65cfad
toomyem <toomyem@toomyem.net>
2014-01-21 14:54:05 +0100

    Added logging mechanism

--- config-sample.py
+++ config-sample.py
@@ -1,6 +1,7 @@
 
 ################################################################
 debug_mode = True   # in debug mode nothing will be sold!
+log_filename = ""   # file name for log
 
 #################################################################
 # data needed to access cex.io api
@@ -10,18 +11,18 @@ secret = ""
 
 #################################################################
 # settings about trade
-ghs     = 1.3814   # number of ghs to be sold on price drop
-limit   = 0.045    # limit for stop loss
-gap     = 0.0001   # window size
+ghs     = 1.40      # number of ghs to be sold on price drop
+limit   = 0.0462    # limit for stop loss
+gap     = 0.0001    # window size
 maximum = limit+gap # top price for trailing stop loss
-delay   = 10       # delay in secs between steps
-window  = 120      # time window for mean price calculation
+delay   = 10        # delay in secs between steps
+window  = 120       # time window for mean price calculation
 
 ################################################################
 # configuraton for mail notifier
-mail_enabled = not debug_mode
-mail_srv     = ""  # mail server address, for example smtp.gmail.com
-mail_user    = ""  # user name on mail server
-mail_pass    = ""  # password for that user
-mail_to      = ""  # who shall receive the information
+mail_enabled = False
+mail_srv     = "smtp.gmail.com"
+mail_user    = ""
+mail_pass    = ""
+mail_to      = ""
 
--- trader.py
+++ trader.py
@@ -6,6 +6,16 @@ import notify
 
 api = cexapi.CexApi(config.username, config.apikey, config.secret)
 
+def get_ts():
+    return "[" + time.strftime("%Y/%m/%d %H:%M:%S") + "]"
+
+def log(msg):
+  print get_ts(), msg
+  if config.log_filename:
+    f = open(config.log_filename, "a")
+    f.write(get_ts() + " " + msg + "\n")
+    f.close()
+
 def get_mean_price(orders):
   p = [o['price'] for o in orders]
   return sum(p)/len(p)
@@ -13,21 +23,19 @@ def get_mean_price(orders):
 def sell_ghs(amount, price):
     if config.debug_mode:
       msg = "You should sell %f GHS for %0.8f BTC on cex.io" % (amount, price)
-      print get_ts(), msg
+      log(msg)
       if config.mail_enabled: notify.send_mail(msg)
       return False
     else:
       msg = "Attempting to sell %f GHS for %0.8f BTC on cex.io" % (amount, price)
-      print get_ts(), msg
-      print api.place_sell_order(amount, price)
-      if config.mail_enabled: notify.send_mail(msg)
+      log(msg)
+      r = api.place_sell_order(amount, price)
+      log(r)
+      if config.mail_enabled: notify.send_mail(msg + "\r\n" + r)
       return True
 
 def to8(num):
     return "%0.8f" % round(num, 8)
-
-def get_ts():
-    return "[" + time.strftime("%Y/%m/%d %H:%M:%S") + "]"
 
 def trailing_stop_loss(limit, maximum, delay):
   last_price = 0
@@ -46,7 +54,7 @@ def trailing_stop_loss(limit, maximum, delay):
     diff = price - start_limit
 
     if price < limit:
-      print get_ts(), "price went down to:", to8(price), "limit:", to8(limit)
+      log("price went down to: " + to8(price) + " limit: " + to8(limit))
       if sell_ghs(config.ghs, start_limit):
         break
 
@@ -54,7 +62,7 @@ def trailing_stop_loss(limit, maximum, delay):
       limit += (price - maximum)
       maximum = price
 
-    print get_ts(), "limit:", to8(limit), "price:", to8(price), "max:", to8(maximum), "diff:", to8(diff), "delta:", to8(delta)
+    log("limit: " + to8(limit) + " price: " + to8(price) + " max: " + to8(maximum) + " diff: " + to8(diff) + " delta: " + to8(delta))
 
     last_price = price
     time.sleep(config.delay)
